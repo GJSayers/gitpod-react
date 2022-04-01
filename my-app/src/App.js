d8156c5fa7e6f75afb97b4790ae5fe9171a91e21
GJSayers <gemma.sayers1@btinternet.com>
2022-04-01 19:42:47 +0000

    Add new file to demonstrate calback function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 // This componenet is a parent component
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Fiona" age="32" />
       <StatefulGreeting greeting="I'm a stateful class component!" name="Fiona"/>
+      <StatefulGreetingWithCallback greeting="I'm a stateful class component!" name="Fiona"/>
     </div>
   );
 }
