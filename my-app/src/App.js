f4835c78e2844883d5a9e5ab816c2f3c609c32fb
GJSayers <gemma.sayers1@btinternet.com>
2022-04-01 18:55:53 +0000

    Add StatefulGreeting component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-
+import StatefulGreeting from './components/StatefulGreeting';
 
 // This componenet is a parent component
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Fiona" age="32" />
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Fiona"/>
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class StatefulGreeting extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    render() {
+        return (
+        <div>
+        <h1>Hello {this.props.greeting} My name is {this.props.name}</h1>
+        <button>{this.state.buttonText}</button>
+        </div>
+        )
+    }
+
+}
+
+export default StatefulGreeting;
